dddc71c7651b626d8046cefc42b125fde30b9cd6
Sumeet Jain <sumeet@sumeetjain.com>
2013-06-20 14:36:00 -0700

    Rails, no CoffeeScript.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WdiTasks::Application.config.secret_token = '0bd770d4cdcfd4ecf9b66f15350e58bea12e1132f40b4f12895483764557a5ae50f42033589aecae8ad05cb54f8885d0ea5d951f910f06bcfab51832cb162170'
