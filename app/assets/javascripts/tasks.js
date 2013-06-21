530c47fc19b97d06cabe508119b7c2f6cf469621
Sumeet Jain <sumeet@sumeetjain.com>
2013-06-21 11:20:25 -0700

    Hide Update button for newly added tasks

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+  // On page-load, I hide all Edit Task submit buttons.
   $(".edit_task input[type=submit]").remove();
 
   $("body").on("click", ".edit_task input[type=checkbox]", function(e){
